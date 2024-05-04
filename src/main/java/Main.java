5c909cc00871055325441a5caa869ae46cb4a5f7
Rami <84153491+ramii3@users.noreply.github.com>
2024-05-04 12:09:44 +0200

    comment added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,21 @@
 public class Main {
 
-    public static void main(String[] args) {
-        Factory factory = new Factory();
+  public static void main(String[] args) {
+    Factory factory = new Factory();
 
-        Car car1 = new Car("red", "BMW", 5);
-        Car car2 = new Car("green", "Audi", 3);
-        Car car3 = new Car("blue", "VW", 3);
+    Car car1 = new Car("red", "BMW", 5);
+    Car car2 = new Car("green", "Audi", 3);
+    Car car3 = new Car("blue", "VW", 3);
 
-        Car car4 = new Car("yellow", "Renault", 4);
+    Car car4 = new Car("yellow", "Renault", 4);
 
-        //An dieser Stelle befindet sich nun ein Array
-        //Dieses Array wird mit einem Sortieralgorithmus sortiert
-        //Optimaler Code Yippie
+    // An dieser Stelle befindet sich nun ein Array
+    // Dieses Array wird mit einem Sortieralgorithmus sortiert
+    // Optimaler Code Yippie
 
-        factory.buildCar(car1);
-        factory.buildCar(car2);
+    factory.buildCar(car1);
+    factory.buildCar(car2);
 
-    }
+    // This is a comment added in the main branch (conflicts with removal)
+  }
 }
-
